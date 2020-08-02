74a5e7cd3ea35922ce76e0b6594e205d76e36d9c
Nate Koike <nkoike1998@gmail.com>
2020-08-03 06:22:47 +0900

    wrote a function to run a single test on the algorithm

--- timing.js
+++ timing.js
@@ -8,10 +8,31 @@ const date = new Date();
 
 // make an array of a specified size full of random numbers and run the
 // algorithm on that array
-function testAlgo(size, algo) {
+// this function has a default test that ends immediately
+function singleTest(size, algo = _ => {}) {
   // make a test array
   test = {};
 
   // fill the array with random numbers
   for (var i = 0; i < size; i++) test.push(Math.random());
+
+  // get the time at the start of the algorithm
+  startTime = date.getTime();
+
+  // run the algorithm on the test array
+  algo(test);
+
+  // get the time at the end of the algorithm
+  endTime = date.getTime();
+
+  // find the change in time
+  elapsed = endTime - startTime;
+
+  return elapsed;
 }
+
+// run a series of tests on the algorithm
+function testAlgo(algo) {}
+
+// // debugging code
+// console.log(singleTest(0));
