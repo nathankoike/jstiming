45361fade072e7c2992f6ccf6edac4ed5caab3c7
Nate Koike <nkoike1998@gmail.com>
2020-08-03 06:16:57 +0900

    started writing a test script

---
+++ timing.js
@@ -0,0 +1,17 @@
+/*
+Auth: Nate Koike
+Date: 2 August 2020
+Desc: automatically tests the timing of an array algorithm
+*/
+
+const date = new Date();
+
+// make an array of a specified size full of random numbers and run the
+// algorithm on that array
+function testAlgo(size, algo) {
+  // make a test array
+  test = {};
+
+  // fill the array with random numbers
+  for (var i = 0; i < size; i++) test.push(Math.random());
+}
