7af021fa74604c7cb18687775dd6ab3df71ba9b5
Nate Koike <nkoike1998@gmail.com>
2020-08-03 07:01:33 +0900

    debugging

--- merge.js
+++ merge.js
@@ -12,17 +12,37 @@ function sortCondition(first, last) {
 
 // merge two arrays into one
 // this function has a condition that always returns True
-function merge(first, last, cond = (_, _) => True) {
+function merge(first, last, cond = (x, y) => true) {
   // the final array
   var result = [];
 
+  console.log(first);
+
   // while both arrays are full
-  while (first != [] && last != []) {
+  while (first.length > 1) {
     // if the first item of the first array meets the condition
     if (cond(first[0], last[0])) {
+      // add the first element of the first array to the result
       result.push(first[0]);
+
+      // remove the first element from the array
+      first = first.slice(1);
+    } else {
+      // add the first element of the last array to the result
+      result.push(last[0]);
+
+      // remove the first element from the array
+      last = last.slice(1);
     }
   }
+
+  // one array is empty
+  // if the first array is empty
+  if (first == [])
+    // dump every element in the last array into the result
+    for (; last != []; last = last.slice(1)) result.push(last[0]);
+  // dump every element in the first array into the result
+  else for (; first != []; first = first.slice(1)) result.push(first[0]);
 }
 
 function sort(ary) {
@@ -40,3 +60,5 @@ function sort(ary) {
   // merge the front and rear of the array and return
   return merge(front, rear);
 }
+
+console.log(sort([2, 3, 1, 5, 4, 6]));
